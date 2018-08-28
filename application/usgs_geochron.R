2558828fd0a4cf39d3d39e3c5919700da6a02a6e
aazaff <aazaff@gmail.com>
2018-08-28 07:58:56 -0700

    greatly simplified the script because it now uses geocarrot beta

--- application/usgs_geochron.R
+++ application/usgs_geochron.R
@@ -6,7 +6,7 @@
 
 ######################################### Load Required Libraries ###########################################
 # Save and print the app start time
-Start<-print(Sys.time())
+Start<-print(paste("Starting application",Sys.time()))
 
 # If running from UW-Madison
 # Load or install the doParallel package
@@ -19,6 +19,19 @@ if (suppressWarnings(require("doParallel"))==FALSE) {
 if (suppressWarnings(require("RPostgreSQL"))==FALSE) {
         install.packages("RPostgreSQL",repos="http://cran.cnr.berkeley.edu/");
         library("RPostgreSQL");
+        }
+
+# Load or install devtools. This is needed to get the geocarrot beta. This step can be removed once geocarrot is live.
+if (suppressWarnings(require("devtools"))==FALSE) {
+        install.packages("devtools",repos="http://cran.cnr.berkeley.edu/");
+        library("devtools");
+        }
+
+# Install the geocarrot package from github
+# This currently requires devtools... grrr
+if (suppressWarnings(require("geocarrot"))==FALSE) {
+        devtools::install_github("aazaff/geocarrot");
+        library("geocarrot");
         }
 
 # Start a cluster for multicore, 3 by default or higher if passed as command line argument
@@ -46,94 +59,34 @@ Connection <- dbConnect(Driver, dbname = Credentials["database:",], host = Crede
 # Query the sentences fro postgresql
 DeepDiveData<-dbGetQuery(Connection,"SELECT docid, sentid, words, poses, dep_paths, dep_parents FROM nlp_sentences_352")
 
-# print current status to terminal 
-print(paste("Load postgres tables",Sys.time()))
-
 #############################################################################################################
 ###################################### NNP CLUSTER FUNCTIONS, GEODEEPDIVE ###################################
 #############################################################################################################
-# A function to find proper noun clusters
-findCluster<-function(Sentence,Parameters=c("words","poses"),Pose="NNP") {
-        ParsedSentence<-parseSentence(Sentence,Parameters)
-        if(all(is.na(ParsedSentence))) {return(setNames(c(Sentence["docid"], Sentence["sentid"],"parsing error"), c("docid", "sentid", "Proper")))}
-        FindConsecutive<-findConsecutive(which(ParsedSentence["poses",]==Pose))
-        Proper<-sapply(FindConsecutive,function(x) paste(unname(ParsedSentence["words",x]),collapse=" "))
-        Proper<-unname(cbind(Sentence["docid"],Sentence["sentid"],Proper))
-        colnames(Proper)<-c("docid","sentid","Proper") 
-        return(Proper)
-        }
-
-# Sees if numbers are consecutive
-findConsecutive<-function(NumericSequence) {
-        Breaks<-c(0,which(diff(NumericSequence)!=1),length(NumericSequence))
-        ConsecutiveList<-lapply(seq(length(Breaks)-1),function(x) NumericSequence[(Breaks[x]+1):Breaks[x+1]])
-        return(ConsecutiveList)
-        }
-
-# Parse sentences
-parseSentence<-function(Sentence,Parameters=c("words","dep_paths","dep_parents")) {
-        Sentence<-setNames(cleanPunctuation(Sentence),names(Sentence))
-        if ("words"%in%names(Sentence)) {Sentence["words"]<-trueCommas(Sentence["words"])}
-        WordsMatrix<-sapply(Sentence[Parameters],function(x) strsplit(x,","))
-        if (sum(diff(sapply(WordsMatrix,length)))!=0) {return(NA)}
-        WordsMatrix<-do.call(rbind,WordsMatrix)
-        WordsMatrix[which(WordsMatrix=="COMMASUB")]<-","
-        WordsMatrix[which(WordsMatrix=="SPACESUB")]<-""
-        colnames(WordsMatrix)<-1:ncol(WordsMatrix)
-        return(WordsMatrix)
-        }
-                            
-# R confuses 2,000,381 in a PostgreSQL array as 2 000 381, this function will convert those cases to 2000381.  
-trueCommas<-function(Words) {
-        InsideQuotes<-regmatches(Words, gregexpr('"[^"]*"',Words))[[1]]
-        if (length(InsideQuotes)<1) {return(Words)}
-        Replacements<-gsub(",","",InsideQuotes)
-        for (i in 1:length(InsideQuotes)) {
-                Words<-noquote(gsub(InsideQuotes[i],Replacements[i],Words))
-                }
-        return(Words)
-        }
-
-# Remove or replace problematic punctuation
-# Even though this is redundnat with trueCommas it applies to more fields
-cleanPunctuation<-function(Sentence) {
-        Sentence<-gsub("\"\"","SPACESUB",Sentence)
-        Sentence<-gsub("\",\"","COMMASUB",Sentence) 
-        Sentence<-gsub("\\{|\\}","",Sentence)
-        Sentence<-gsub("-LRB-","(",Sentence)
-        Sentence<-gsub("-RRB-",")",Sentence)
-        Sentence<-gsub("-LCB-","{",Sentence)
-        Sentence<-gsub("-RCB-","}",Sentence)
-        return(Sentence)
-        }
+# No sentences at this time.
 
 ######################################## NNP CLUSTER SCRIPT, GEODEEPDIVE ####################################
 # Print the current stats to terminal
 print(paste("Distribute all functions to each core",Sys.time()))
 
 # Pass the functions to the cluster
-clusterExport(cl=Cluster,varlist=c("parseSentence","findConsecutive","findCluster","cleanPunctuation","trueCommas"))
-
-# print current status to terminal
-print(paste("Clean up the punctuation",Sys.time()))
-
-# Apply the clean punctuation to each sentence type
-CleanedSentences<-parApply(Cluster,DeepDiveData,1,cleanPunctuation)
+clusterExport(cl=Cluster,varlist=c("parseSentence","consecutivePoses"))
 
 # print current status to terminal
 print(paste("Extract all proper noun clusters",Sys.time()))
 
 # Generate the proper noun strings
-ProperList<-parApply(Cluster,CleanedSentences,1,findCluster)
+ProperList<-parApply(Cluster,DeepDiveData,1,consecutivePoses)
 # Stop the cluster
 stopCluster(Cluster)
 
+# print current status to terminal
+print(paste("Extraction complete. Writing output",Sys.time()))
+
 # Collapse the list into a character matrix
 ProperMatrix<-na.omit(do.call(rbind,ProperList))
-colnames(ProperMatrix)<-c("docid","sentid","Proper")
 
 # Remove the "NA"'s
-ProperMatrix<-subset(ProperMatrix,ProperMatrix[,"Proper"]!="NA")
+ProperMatrix<-subset(ProperMatrix,ProperMatrix[,"NNP"]!="NA")
 
 # Set directory for output
 OutputPath<-paste0(getwd(),"/output")
@@ -143,11 +96,8 @@ if (!dir.exists(OutputPath)) {
         dir.create(OutputPath)
         }
   
-# Clear any old output files
-unlink("*")
-
 # Write csv output files
 write.csv(ProperMatrix, paste(OutputPath,"ProperMatrix.csv",sep="/"))
 
 # COMPLETE
-print(paste("Complete",Sys.time()))
+print(paste("Application Complete",Sys.time()))
